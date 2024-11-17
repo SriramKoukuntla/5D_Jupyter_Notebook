f2c0002d7d2669e9018c7fb6050ece90b37ab6e0
MacAjwani <mayankajwani@gmail.com>
2024-11-17 05:56:39 -0600

    completed project routes

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -20,9 +20,9 @@ const router = express.Router();
  */
 router.post("/createProject", async (req, res) => {
 	try {
+		const supabase = getDatabaseConnection();
 		const name = req.body.name;
 		const desc = req.body.desc;
-		const supabase = getDatabaseConnection();
 		const root = {
 			code: "",
 			output: "",
@@ -48,11 +48,95 @@ router.post("/createProject", async (req, res) => {
 	}
 });
 
-// router.patch("/updateParentNode", async (req, res) => {
-//     try {
-//         const newChildren = req.body.children;
-//         const {code, output, images, description, stage}
-//     }
-// });
+/**
+ * Body of request must be a JSON object containing any number of the three fields, "name", "desc", "tree". Must pass in request 
+ * Returns a JSON object containing a field called "message" with a success message and a list called "data", where the sole entry is the new version of the updated project
+ * Ex:
+ * 
+ * Request: localhost:8080/api/project/updateProject/1
+ * {
+    "desc": "test"
+ * }
+	Response:
+	{
+		"message": "Project updated successfully",
+		"data": [
+			{
+				"id": 1,
+				"name": "first project",
+				"desc": "test",
+				"tree": "{\"code\":\"\",\"output\":\"\",\"images\":[],\"desc\":\"\",\"stage\":\"\",\"children\":[]}"
+			}
+		]
+	}
+ */
+router.patch("/updateProject/:id", async (req, res) => {
+	try {
+		const supabase = getDatabaseConnection();
+		const id = req.params.id;
+		if (!id) {
+			throw new Error("No id provided!");
+		}
+
+		const { data, error } = await supabase
+			.from("projects")
+			.update(req.body)
+			.eq("id", id)
+			.select();
+		if (!data) {
+			throw new Error(error);
+		}
+		return res.status(200).json({
+			message: "Project updated successfully",
+			data,
+		});
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+/**
+ * Must pass id in request through params
+ * Returns a JSON object containing a field called "message" with a success message and a list called "data", where the sole entry is the deleted project
+ * Ex:
+ * 
+ * Request: localhost:8080/api/project/deleteProject/1
+	Response:
+	{
+		"message": "Project deleted successfully",
+		"data": [
+			{
+				"id": 1,
+				"name": "first project",
+				"desc": "test",
+				"tree": "{\"code\":\"\",\"output\":\"\",\"images\":[],\"desc\":\"\",\"stage\":\"\",\"children\":[]}"
+			}
+		]
+	}
+ */
+router.delete("/deleteProject/:id", async (req, res) => {
+	try {
+		const supabase = getDatabaseConnection();
+		const id = req.params.id;
+		if (!id) {
+			throw new Error("No id provided!");
+		}
+
+		const { data, error } = await supabase
+			.from("projects")
+			.delete()
+			.eq("id", id)
+			.select();
+		if (!data) {
+			throw new Error(error);
+		}
+		return res.status(200).json({
+			message: "Project deleted successfully",
+			data,
+		});
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
 
 export default router;
