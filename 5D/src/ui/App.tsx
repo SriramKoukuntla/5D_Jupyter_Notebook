f4e9d576640aba6cf681cfe0a0b8f9f36d04d1df
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2025-01-18 01:57:22 -0600

    Some Basic user resizable divs

--- 5D/src/ui/App.tsx
+++ 5D/src/ui/App.tsx
@@ -1,31 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
+import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-      <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        This is the point where all the boilercode is basically done. I am making a commit here so that I can fork this part if I ever want to make another desktop electron app.
-      </p>
-    </>
-  )
+    <div className="App">
+      <PanelGroup direction="horizontal">
+        <Panel>
+          left
+        </Panel>
+        <PanelResizeHandle />
+        <Panel>
+          <PanelGroup direction="vertical">
+            <Panel>
+              top
+            </Panel>
+            <PanelResizeHandle />
+            <Panel>
+              <PanelGroup direction="horizontal">
+                <Panel>
+                  left
+                </Panel>
+                <PanelResizeHandle />
+                <Panel>
+                  right
+                </Panel>
+              </PanelGroup>
+            </Panel>
+          </PanelGroup>
+        </Panel>
+        <PanelResizeHandle />
+      </PanelGroup>
+    </div>
+  );
 }
 
-export default App
+export default App;
